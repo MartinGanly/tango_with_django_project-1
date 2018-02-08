83e7f3166952370564d7980f23bd54a18013dda7
Liam Brodie <2198998b@student.gla.ac.uk>
2018-02-08 15:24:55 +0000

    Chapter 8

--- rango/forms.py
+++ rango/forms.py
@@ -21,6 +21,17 @@ class PageForm(forms.ModelForm):
                          help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenINput(), initial=0)
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+            return cleaned_data
+
+
     class Meta:
         #provide an association between the modelform and a model
         model = Page
